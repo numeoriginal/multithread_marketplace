8827780bb9e8e72261d75a05945804aede323282
Apathy <constantin1337@yahoo.com>
2021-03-27 10:16:54 -0700

    constructor

---
+++ producer.py
@@ -0,0 +1,47 @@
+"""
+This module represents the Producer.
+
+Computer Systems Architecture Course
+Assignment 1
+March 2021
+"""
+
+from threading import Thread
+import json
+
+
+class Producer(Thread):
+    """
+    Class that represents a producer.
+    """
+
+    def __init__(self, products, marketplace, republish_wait_time, **kwargs):
+        """
+        Constructor.
+
+        @type products: List()
+        @param products: a list of products that the producer will produce
+
+        @type marketplace: Marketplace
+        @param marketplace: a reference to the marketplace
+
+        @type republish_wait_time: Time
+        @param republish_wait_time: the number of seconds that a producer must
+        wait until the marketplace becomes available
+
+        @type kwargs:
+        @param kwargs: other arguments that are passed to the Thread's __init__()
+        """
+
+        super(Producer, self).__init__()
+        self.products = products
+        self.marketplace = marketplace
+        self.republish_wait_time = republish_wait_time
+        self.kwargs = kwargs
+        print(kwargs)
+
+    def run(self):
+        self.id = self.marketplace.register_producer()
+        for it in self.products:
+            print(it)
+            #self.marketplace.publish()
